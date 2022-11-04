8d94424714ae26f3d33b9f86cdd642b376c513e6
Matthis Guilleray <mat.guilleray@gmail.com>
2022-11-04 09:35:15 +0100

    Commit of 04/11/2022 revising the histogram part

--- Classifier.m
+++ Classifier.m
@@ -1,34 +1,36 @@
 function Classifier
+
+    main()
+
+end
+
+function main()
     %{
     Goal : Run the test of the classifier
     Parameters : 
         None
-    Return : 
+    Return :
         None
     %}
     global nbWordVOCBuilding 
     global nbImagesVOCBuilding
     global nbImagesClsTraining 
-    global nbThresholdMatchFct
-    global maxIterationVOCBuilding
-    global distanceAlgoVOCBuilding
     global nbImagesTesting
     global nbFdImages
     global fdAlgo
     global trainingSet
+    global treshold
+
+
     % Parameters : 
     nbWordVOCBuilding = 200; % Vocabulary : K parameter in Kmeans in VOCBuilding
     nbImagesVOCBuilding = -1; % Number of images processed in VOCBuilding, Max value at -1
     nbImagesClsTraining = -1; % Number of images used to train the classifier, Max value at -1
-    nbThresholdMatchFct = 5; % Percentage needed to match between two features
     nbImagesTesting = -1; % Number of images used to test the whole algo, Max value at -1
-    maxIterationVOCBuilding = 10000000; % Maximum iteration in the Kmean algorithm
-    distanceAlgoVOCBuilding = 'sqeuclidean'; % Distance method used in the Kmean algo
     nbFdImages = 300; % Number of features descriptors per images
     fdAlgo = "SIFT"; % Method used to find the features : "SIFT", "ORB"
     trainingSet = "train"; % Which set is used
-
-    AllfdAlgo = ["SIFT", "ORB"]; % List of all the features descriptors implemented
+    treshold = 120000;
     
     % End of parameters
 
@@ -37,11 +39,6 @@ function Classifier
 
     % initialize VOC options
     VOCinit;
-
-    % Building of the vocabulary
-    % getAllVOC(VOCopts, AllfdAlgo);
-    
-
 
     % train and test classifier for each class
     for i=1:VOCopts.nclasses
@@ -54,27 +51,11 @@ function Classifier
         hour = fix(clock);
         fprintf("%dH:%dM:%dS - End of Testings\n", hour(4),hour(5),hour(6))
         [fp,tp,auc]=VOCroc(VOCopts,'comp1',cls,true);   % compute and display ROC
-        
         if i<VOCopts.nclasses
             fprintf('press any key to continue with next class...\n');
             pause;
         end
     end
-end
-function getAllVOC(VOCopts, fdAlgo)
-    global trainingSet
-    [ids,~]=textread(sprintf(VOCopts.clsimgsetpath,"bicycle",trainingSet),'%s %d');
-    hour = fix(clock);
-    fprintf("%dH:%dM:%dS - General Vocabulary building\n", hour(4),hour(5),hour(6))
-    for i=1:length(fdAlgo)
-        hour = fix(clock);
-        fprintf("%dH:%dM:%dS - %s\n", hour(4),hour(5),hour(6), fdAlgo(i))
-        VOCBuilding(VOCopts, ids, fdAlgo(i)); % get the VOC
-    end
-    hour = fix(clock);
-    fprintf("%dH:%dM:%dS - General Vocabulary building finished\n", hour(4),hour(5),hour(6))
-    error("The VOC have been successfully readed, sorted and stored")
-
 end
 
 function classifier = train(VOCopts,cls)
@@ -87,15 +68,14 @@ function classifier = train(VOCopts,cls)
         classifier : This classifier object will be used to make the classifier works
     %}
     global fdAlgo
-    global trainingSet
 
     % Load the name of the image for the selected set and the selected class
     [ids,classifier.gt]=textread(sprintf(VOCopts.clsimgsetpath,cls,"train"),'%s %d');
-    % We should read the Buck of word here
+    % VOCabulary Building
     classifier.FD = VOCBuilding(VOCopts, ids, fdAlgo);
     hour = fix(clock);
     fprintf("%dH:%dM:%dS - Classifier training\n", hour(4),hour(5),hour(6))
-    [ids,classifier.gt]=textread(sprintf(VOCopts.clsimgsetpath,cls,"val"),'%s %d');
+    % Training of the model
     classifier.model = ClassifierTraining(VOCopts, classifier, ids);
 end
 
@@ -108,8 +88,6 @@ function c = VOCBuilding(VOCopts, ids, fdAlgo)
     Return : 
         f : a nbWord of features (for Sift : nbWord by 128)
     %}
-    global maxIterationVOCBuilding
-    global distanceAlgoVOCBuilding
     global nbImagesVOCBuilding
     global nbWordVOCBuilding
     tic; % Fct to start the timer
@@ -119,6 +97,7 @@ function c = VOCBuilding(VOCopts, ids, fdAlgo)
     drawnow;
 
     try 
+        error("defecting to reading images")
         load(sprintf(VOCopts.exVOCpath,fdAlgo),'c');
         fprintf("Skipping the reading of the images \n")
 
@@ -133,22 +112,25 @@ function c = VOCBuilding(VOCopts, ids, fdAlgo)
                 drawnow;
                 tic;
             end
+            try
+                load(sprintf(VOCopts.exfdpath,ids{i}),'fd');
+            catch
                 % Read the image, extract the features, and stores the features
                 I=imread(sprintf(VOCopts.imgpath,ids{i})); % Lecture de l'image
                 fd = extractfd(I, fdAlgo); % Extraction of features, return a tab of nbFeat * LenghtPerFeature (ex SIFT lenngth = 128)
                 % fd = gpuArray(fd); % Using GPU | comment to use the CPU
                 % features in a file previous line commented bc I try to save
-                % the whole model in different states
+                % the whole model in different states 
+                save(sprintf(VOCopts.exfdpath,ids{i}),'fd');
+            end
             if i == 1 % In the case of the first iteration, where fd is undefined
                 AllFeatures = fd; % Normal way
-                % AllFeatures = gpuArray(fd); % Using GPU | comment to use the
-                % CPU
             else
-                AllFeatures = [AllFeatures; fd]; 
+                AllFeatures = [AllFeatures, fd]; 
             end
             
         end
-        
+         
         % Problem too much time, consider lowering the values
         % Maybe just ignore the warning, or sort the fd directly in extractfeature but can cause some problem
         % Maybe just too much value
@@ -159,12 +141,10 @@ function c = VOCBuilding(VOCopts, ids, fdAlgo)
         % eucD = pdist(AllFeatures,'euclidean');
         % c = linkage(eucD,'average');
         % kmean clustering
-        AllFeatures = transpose(AllFeatures);
-        [c, ~] = vl_kmeans(AllFeatures, nbWordVOCBuilding, 'Initialization', 'plusplus', 'Algorithm', 'Lloyd');
+        [c, ~] = vl_ikmeans(AllFeatures, nbWordVOCBuilding, 'Verbose');
         hour = fix(clock);
         fprintf("%dH:%dM:%dS - ", hour(4),hour(5),hour(6))
         fprintf("Algo finished\n")
-        c = transpose(c)
         % We are only interested in C, which is a vector of K by 128
         save(sprintf(VOCopts.exVOCpath,num2str(fdAlgo)),'c'); % Save of the voc
     end
@@ -188,24 +168,23 @@ function model = ClassifierTraining(VOCopts, classifier, ids)
         nbImagesClsTraining = length(ids); % Number of image in the classifier training
     end
     nbWord = size(classifier.FD);
-    nbWord = nbWord(1);
+    nbWord = nbWord(2);
     X = zeros(nbImagesClsTraining, nbWord); % The histogram of all the trainning images
     y = zeros(nbImagesClsTraining, 1); % The Binary value of the image wether is from the class or not
     hour = fix(clock);
     fprintf("%dH:%dM:%dS - Reading of the images\n", hour(4),hour(5),hour(6))
     for i=1:nbImagesClsTraining
-        I=imread(sprintf(VOCopts.imgpath,ids{i}));
-        histogram = SearchWord(I,classifier.FD);
-        X(i, 1:nbWord) = histogram;
+        histogram = SearchWord(VOCopts, ids{i},classifier.FD);
+        X(i, 1:nbWord) = transpose(histogram);
         y(i,1) = classifier.gt(i); % Wether it is or not from the searched class
     end
     hour = fix(clock);
     fprintf("%dH:%dM:%dS - Training of the model\n", hour(4),hour(5),hour(6))
 
-    % model = fitcsvm(X,y); % Training of the SVM model
-    model =fitcsvm(X,y,'OptimizeHyperparameters','auto', ...
-    'HyperparameterOptimizationOptions',struct('AcquisitionFunctionName', ...
-    'expected-improvement-plus'))
+    model = fitcsvm(X,y); % Training of the SVM model
+%     model =fitcsvm(X,y,'OptimizeHyperparameters','auto', ...
+%     'HyperparameterOptimizationOptions',struct('AcquisitionFunctionName', ...
+%     'expected-improvement-plus'))
 
     hour = fix(clock);
     fprintf("%dH:%dM:%dS - End of training of the model\n", hour(4),hour(5),hour(6))
@@ -246,8 +225,7 @@ function test(VOCopts,cls,classifier)
         end
         
         % Read the image and compute the histogram
-        I=imread(sprintf(VOCopts.imgpath,ids{i}));
-        histogram = SearchWord(I, classifier.FD);
+        histogram = SearchWord(VOCopts, ids{i}, classifier.FD);
 
         % compute confidence of positive classification
         c=classify(VOCopts,classifier,histogram);
@@ -261,35 +239,81 @@ function test(VOCopts,cls,classifier)
 
 end
 
-function histogram = SearchWord(I, Bow)
+function total = matchFeaturesLocal(ft1, ft2, fdAlgolc)
+%     ft2 tab de features
+    global fdAlgo
+    if isnumeric(fdAlgolc)
+        fdAlgolc = fdAlgo;
+    end
+    global treshold
+    min=9999999;
+    total = 0;
+    sz = size(ft2);
+    for i=1:sz(2)
+        if fdAlgolc == "RGB" 
+            if ft1 == ft2(i)
+                total = total + 1;
+            end
+        else
+            % ip = matchFeatures(transpose(ft1), transpose(ft2(i,:))); %
+            % Error it will furnish a 0 by 2 matrix even if the result is
+            % the same
+            [dist,sc]= vl_ubcmatch(uint8(ft1),ft2(:,i)); % sc is theeuclidiandistancebtwpoints
+            if sc < treshold
+                total = total + 1;
+            end
+            if  min>sc
+                min=sc;
+            end
+        end
+    end
+end
+
+function histogram = SearchWord(VOCopts, ids, Bow)
     %{
     Goal : for a given image, and a given buck of word, this function will search those words in the image and return a histogram
     Should work, tested on basic situation
     Parameters : 
-        I : The Image to process    
+        ids : Name of theimage
         Bow : Buck of word an array of k by 128 for SIFT
     Return : 
         histogram : The histogram of the BOW (normalized)
     %}
-    global nbThresholdMatchFct
     % Parameters : 
-    treshold = nbThresholdMatchFct; % Threshold for the matching
-
-    fd=extractfd(I, -1);
-    sz = size(Bow);
+    try
+        load(sprintf(VOCopts.exfdpath,ids),'fd');
+    catch
+        I=imread(sprintf(VOCopts.imgpath,ids));
+        fd = extractfd(I, -1);
+        save(sprintf(VOCopts.exfdpath,ids),'fd');
+    end
+    szBw = size(Bow);
+    histogram = zeros(szBw(2),1);
+    cum = 0;
     
-    indexPairs = matchFeatures(Bow, fd, MatchThreshold=treshold);
-    % Pair of 2 column to match between features
-    histogram = zeros(1,sz(1));
-    % Adding 1 for each occurence in the indexPairs variable
-    szIp = size(indexPairs);
-    for i=1:szIp(1)
-        histogram(1,indexPairs(i,1)) = histogram(1,indexPairs(i,1)) + 1;
-        
+    for i=1:szBw(2)
+        histogram(i) = matchFeaturesLocal(Bow(:,i), fd, -1);
+        cum = cum + histogram(i);
     end
 
+
+    
+    % Pair of 2 column to match between features
+    
+    % Adding 1 for each occurence in the indexPairs variable
+
     % Normalization
-    histogram = histogram/szIp(1);
+    tmp = zeros(szBw(2),1);
+    for i=1:length(histogram)
+        if histogram(i)~= 0
+            tmp(i) = histogram(i)/cum;
+        else
+            tmp(i) = 0;
+        end
+
+    end
+    histogram = tmp;
+    
 
 end
 
@@ -301,7 +325,7 @@ function I = preProcessingImages(I)
     Return : 
         I : The modified images, it have to be in 2D, monochannel with the function RGB2GRAY
     %}
-
+    
     I = rgb2gray(I);
     % I = edge(I,"log", [], 0.2);
 
@@ -319,27 +343,29 @@ function fd = extractfd(I, fdAlgolc)
     global fdAlgo
     if isnumeric(fdAlgolc)
         fdAlgolc = fdAlgo;
-
     end
-
-    fd = [];
+         
+    fd = [];                       
     I = preProcessingImages(I);
     if fdAlgolc == "SIFT"
         I = single(I);
         [f,fd] = vl_sift(I);
-        fd = transpose(fd);
-        fd = double(fd);
+        % fd = transpose(fd);
+        % fd = double(fd);
     elseif fdAlgolc == "ORB"
         points = detectORBFeatures(I);
         fd = points.selectStrongest(nbFdImages);
         fd = extractFeatures(I,fd).Features;
         fd = double(fd);
+    elseif fdAlgolc == "RGB"
+        fd = [[mean(I, "all")]];
     else 
         error("Algo not implemented : "+fdAlgolc)
     end
 end    
 
 function c = classify(VOCopts,classifier,histogram)
+
     %{
     Goal : Use the classifier trained
     Parameters : 
@@ -351,7 +377,7 @@ function c = classify(VOCopts,classifier,histogram)
     %}
     % Here implement the classifier (SVM, Adaboost, etc...)
 
-    [label,score] = predict(classifier.model, histogram);
+    [label,score] = predict(classifier.model, transpose(histogram));
     c = score(2);
 
 end
